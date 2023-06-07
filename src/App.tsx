5596e052a8bbd4cc8daaf1b531b620193a506223
stonasss <luis.b.santos96@gmail.com>
2023-06-07 20:41:19 -0300

    config: adjust vite/tsc configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+export default function HomePage(): any {
+    <div>
+        <h1>Hello</h1>
+    </div>
+}
