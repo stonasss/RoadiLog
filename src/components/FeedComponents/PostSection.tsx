c74cec710daa81f8c1b0ede08ca84207755d58be
stonasss <luis.b.santos96@gmail.com>
2023-06-19 16:50:22 -0300

    feat: posts loading from API

--- src/components/FeedComponents/PostSection.tsx
+++ src/components/FeedComponents/PostSection.tsx
@@ -1,8 +1,28 @@
-import { useState, useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import UserContext from '../../context/UserContext';
 import axios from 'axios';
 
 export default function PostSection(): any {
+    const { userData } = useContext(UserContext)
+    const [userPosts, setUserPosts] = useState([])
+    console.log(userData)
+
+    async function loadFeed(): Promise<any> {
+        await axios.get(`http://localhost:4000/posts/${userData.image.id}`, userData.token)
+            .then((res) => {
+                setUserPosts(res.data.userPosts)
+            })
+            .catch((err) => {
+                alert(err.message);
+            })
+    }
+
+    useEffect(() => {
+        loadFeed()
+    }, [userData]);
+
+    console.log(userPosts)
+
     return (
         <div className="container m-auto pt-20 max-w-xl items-center">
             <div className="header flex flex-row border-b-2 border-slate-700 pb-1 pr-6">
@@ -13,19 +33,25 @@ export default function PostSection(): any {
             </div>
 
             <div className="container">
-                <div className="header mt-6 flex flex-row">
-                    <h1 className="title absolute font-light border-l-8 pl-2 border-slate-700">New milk & honey single</h1>
-                    <h2 className="date relative left-3/4 font-light text-slate-700">19/06/2023</h2>
-                </div>
+                
+                {userPosts.map(post => 
+                <>
+                    <div className="header mt-6 flex flex-row" key={post}>
+                        <h1 className="title absolute font-light border-l-8 pl-2 border-slate-700">{post.title}</h1>
+                        <h2 className="date relative left-3/4 font-light text-slate-700">19/06/2023</h2>
+                    </div>
 
-                <p className="description bg-cyan-400 rounded-2xl p-4 mt-4 font-normal text-cyan-900">This new song should scratch that acoustic emo itch for anyone wondering if they would ever manage to do so in 2023.</p>
+                    <p className="description bg-cyan-400 rounded-2xl p-4 mt-4 font-normal text-cyan-900">{post.description}</p>
 
-                <div className="link mt-4 border-r-8 border-slate-700">
-                    <a 
-                        className="relative left-3/4 font-extrabold"
-                        href="https://milkemohoney.bandcamp.com/track/my-kind-of-animal"
-                    >Check this out!</a>
-                </div>
+                    <div className="link mt-4 border-r-8 border-slate-700">
+                        <a 
+                            className="relative left-3/4 font-extrabold"
+                            href={post.link}
+                        >Check this out!</a>
+                    </div>
+                </>
+                )}
+                
             </div>
         </div>
     )
